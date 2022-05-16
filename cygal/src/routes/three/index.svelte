ad2acd098a0fa8f9f7e5c91f153d0d11c0ae745c
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-05-16 20:47:46 +0200

    Adds three example with routing

---
+++ cygal/src/routes/three/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { onMount } from 'svelte';
+
+let el: HTMLCanvasElement;
+
+onMount(async () => {
+  const { createScene } = await import('./scene');
+  createScene(el);
+});
+</script>
+
+<main>
+  <canvas bind:this="{el}"></canvas>
+</main>
---
+++ cygal/src/routes/three/main.ts
@@ -0,0 +1,8 @@
+import Index from './index.svelte';
+
+const app = new Index({
+  target: document.body,
+  props: {
+    name: 'world',
+  },
+});
