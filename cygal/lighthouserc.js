e9211158fccf8d4b907f8690da41dd122b048477
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-05-16 11:39:14 +0200

    Adds test of running chromium directly

--- cygal/lighthouserc.js
+++ cygal/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      settings: { chromeFlags: '--no-sandbox' },
+      settings: { chromeFlags: '--no-sandbox  --headless --disable-gpu' },
     },
     upload: {
       target: 'temporary-public-storage',
