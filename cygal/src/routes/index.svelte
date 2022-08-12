573a63415590004c1a4d14e396e8f9e81de2d4ac
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-08-12 19:58:01 +0200

    Feat: adds base svelte with vite

--- cygal/src/routes/index.svelte
+++ cygal/src/routes/index.svelte
@@ -1,4 +1,2 @@
-<h1 class="text-4xl underline decoration-rose-400">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
