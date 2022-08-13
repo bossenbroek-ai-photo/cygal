8daa6cbb9ec1fda64a0c4e8cf24ae8bfb836d7f6
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-08-13 17:36:33 +0200

    Feat: builds header with super powers

--- cygal/src/routes/index.svelte
+++ cygal/src/routes/index.svelte
@@ -1,28 +1,5 @@
-<script>
-  const themeColors = ['blue', 'fuschsia', 'gray', 'yellow', 'orange'];
+<script lang="ts">
+  import Header from '../stories/Header.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<h2>With tailwind</h2>
-
-<p>
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-  in culpa qui officia deserunt mollit anim id est laborum."
-</p>
-
-<!--<div>-->
-<!--  <div class="bg-blue"><p class="text-white">blue</p></div>-->
-<!--  <div class="bg-fuschsia"><p class="text-white">fuschsia</p></div>-->
-<!--  <div class="bg-gray"><p class="text-white">gray</p></div>-->
-<!--  <div class="bg-yellow"><p class="text-white">yellow</p></div>-->
-<!--  <div class="bg-orange"><p class="text-white">orange</p></div>-->
-<!--</div>-->
-
-<div>
-  {#each themeColors as color}
-    <div class="bg-{color}"><p class="text-white">{color}</p></div>
-  {/each}
-</div>
+<Header title="Welcome to Anton Bossenbroek Photography" bgIsDark />
--- cygal/src/stories/Header.svelte
+++ cygal/src/stories/Header.svelte
@@ -1,11 +1,33 @@
-<script>
+<script script="ts">
   import './header.css';
+
+  const { title, bgIsDark, showName, showMenu, ...restProps } = $$props;
 </script>
 
+<svelte:head>
+  <title>{title}</title>
+  <meta name="robots" content="noindex nofollow" />
+  <html lang="en" />
+</svelte:head>
+
 <header>
-  <div class="wrapper">
-    <div>
-      <h1>Anton Bossenbroek</h1>
-    </div>
-  </div>
+  {#if showMenu}
+    {#if bgIsDark}
+      <div class="wrapper bg-gray content-start">
+        <div class="h-5 justify-between">
+          {#if showName}
+            <h1 class="text-white m-0">Anton Bossenbroek</h1>
+          {/if}
+        </div>
+      </div>
+    {:else}
+      <div class="wrapper content-start">
+        <div class="h-5 justify-between">
+          {#if showName}
+            <h1 class="m-0">Anton Bossenbroek</h1>
+          {/if}
+        </div>
+      </div>
+    {/if}
+  {/if}
 </header>
