63a8aca511ba59b9097ee3596f6819f3ba9bc9aa
manonja <manon.jacquin@gmail.com>
2022-08-13 13:15:14 +0200

    feat: create about page

---
+++ cygal/src/lib/Card.svelte
@@ -0,0 +1,12 @@
+<script>
+  export const title = 'About';
+  export const desc =
+    'Anton Bossenbroek (b. 1980) is a Dutch / French photographer and entrepreneur based in Amsterdam, The Netherlands. He started his photo career after spending years in corporate life working in artificial intelligence and data science. In his work, he seeks to connect our virtual world to the real world. He looks for obvious and less apparent clues. Drawing inspiration from the abstract and impressionist photography movements to play with structures, patterns, tonalities and subtle details, I seek to explore the dichotomy of the ever-expanding virtual world and the ephemerality of our physical world.';
+</script>
+
+<div class="flex flex-col items-center bg-white md:flex-row md:max-w-xl w-full ">
+  <div class="flex flex-col justify-between p-4 leading-normal">
+    <p class="mb-2 text-3xl font-bold text-gray-800">{title}</p>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{desc}</p>
+  </div>
+</div>
