aca0526e184ddc9ba70548672140ec99b0d628ba
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-08-13 12:15:28 +0200

    Feat: adds color palette

--- cygal/tailwind.config.cjs
+++ cygal/tailwind.config.cjs
@@ -1,8 +1,35 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    screens: {
+      'iphone': {'min': '375px', 'max': '667px'},
+      'ipad': {'min': '820px', 'max': '1180px'},
+      'laptop': '1024px',
+      'large': '1600px'
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      blue: {
+        DEFAULT: '#3e3b6a',
+      },
+      fuschsia: {
+        DEFAULT: '#63455d',
+      },
+      gray: {
+        DEFAULT: '#18152a',
+      },
+      yellow: {
+        DEFAULT: '#ffa502',
+      },
+      orange: {
+        DEFAULT: '#ff892a'
+      }
+
+    }
   },
   plugins: []
 };
