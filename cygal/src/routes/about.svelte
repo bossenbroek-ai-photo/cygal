dcf82304194523a9468e2b4656dbd4bc02867165
Manon <manon.jacquin@gmail.com>
2022-08-13 14:21:13 +0200

    feat: create about page (#6)

---
+++ cygal/src/routes/about.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Header from '../stories/Header.svelte';
+  import Card from '../lib/Card.svelte';
+
+  export const src =
+    'https://images.squarespace-cdn.com/content/v1/60c7a48c6d0b97331ff59d21/1630421946511-I1DGT92KY7GKV7PYWE7R/abossenbroek_profile_photo.jpg?format=1500w';
+  export const alt = 'photo-about';
+</script>
+
+<Header />
+<div class="container mx-auto py-16 px-4">
+  <div class="grid gap-4 grid-cols-2 items-center ">
+    <div class="w-full">
+      <img class="object-cover object-bottom" {src} {alt} />
+    </div>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+      <Card />
+      <a class="pl-4" href="/contact">Get in touch</a>
+    </div>
+  </div>
+</div>
