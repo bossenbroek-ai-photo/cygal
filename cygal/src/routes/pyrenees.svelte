3efdb67b5ac75967ecf52ce73267fd0aa7bb4c4e
manonja <manon.jacquin@gmail.com>
2022-08-13 16:18:24 +0200

    feat: Photo component WIP

---
+++ cygal/src/routes/pyrenees.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Header from '../stories/Header.svelte';
+  import Photo from '../lib/Photo.svelte';
+</script>
+
+<Header />
+<div class="container mx-auto py-16 px-4">
+  <p class="mb-2 text-3xl text-center font-bold text-gray-800 pb-8">Pyrénées</p>
+  <Photo />
+</div>
