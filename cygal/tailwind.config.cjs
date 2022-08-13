28a3e294491dab2533d675795a8503394c78905a
Anton Bossenbroek <anton.bossenbroek@gmail.com>
2022-08-13 14:53:25 +0200

    Adds initial theme components to site (#7)
    * Feat: adds color palette
    
    * Feat: adds fonts and basic font theme set-up
    
    * Feat: adds basic colors to show palette

--- cygal/tailwind.config.cjs
+++ cygal/tailwind.config.cjs
@@ -1,8 +1,39 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    screens: {
+      iphone: { min: '375px', max: '667px' },
+      ipad: { min: '820px', max: '1180px' },
+      laptop: '1024px',
+      large: '1600px'
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      blue: {
+        DEFAULT: '#3e3b6a'
+      },
+      fuschsia: {
+        DEFAULT: '#63455d'
+      },
+      gray: {
+        DEFAULT: '#18152a'
+      },
+      yellow: {
+        DEFAULT: '#ffa502'
+      },
+      orange: {
+        DEFAULT: '#ff892a'
+      }
+    },
+    fontFamily: {
+      macho: ['macho', 'sans-serif'],
+      sans: ['ratio', 'sans-serif']
+    }
   },
   plugins: []
 };
